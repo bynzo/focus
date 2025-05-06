6d0dd141fe777384f5eb488810286f1c2a4b3aa8
Bynzo <arthur.herman.pro@gmail.com>
2025-05-06 13:45:00 -0400

    change icons names

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ const ASSETS = [
   '/styles.css',
   '/app.js',
   '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
