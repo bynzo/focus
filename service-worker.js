9ea98a6173c6bfa57e196093b514fae3bf69de07
Bynzo <arthur.herman.pro@gmail.com>
2025-05-06 15:16:13 -0400

    v2

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = 'focus-app-v1';
+const CACHE_NAME = 'focus-tree-cache-v2';
 const ASSETS = [
-  '/focus/',
-  '/focus/index.html',
-  '/focus/stats.html',
-  '/focus/styles.css',
-  '/focus/app.js',
-  '/focus/manifest.json',
-  '/focus/icons/icon-192x192.png',
-  '/focus/icons/icon-512x512.png'
+  '/',
+  '/index.html?ver=1.1.0',
+  '/stats.html?ver=1.1.0',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png?ver=1.1.0',
+  '/icons/icon-512x512.png?ver=1.1.0'
 ];
 
 self.addEventListener('install', evt => {
+  // Precache new version
   evt.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
@@ -19,13 +20,16 @@ self.addEventListener('install', evt => {
 });
 
 self.addEventListener('activate', evt => {
+  // Remove old caches
   evt.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(old => caches.delete(old))
-      ))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(old => caches.delete(old))
+      )
+    )
+    .then(() => self.clients.claim())
   );
 });
 
